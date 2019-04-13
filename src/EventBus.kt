072f29c56fe58dece6c86db71052f2eaa69f07ba
sbecht <sbecht>
2019-04-13 22:16:08 +0200

    Added option to set priorities for event handlers

--- src/EventBus.kt
+++ src/EventBus.kt
@@ -1,44 +1,89 @@
 package com.sebastianbechtold.eventbus
 
-var eb = EventBus()
+import java.util.*
+import kotlin.Comparator
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
+
+var deb = EventBus()
 
 class EventBus {
 
-	var mListeners: HashMap<Any, Any> = HashMap()
-
-	inline fun <reified T> addListener(noinline listener: (T) -> Unit) {
-
-		var list = mListeners.get(T::class);
-
-		if (list == null) {
-			list = HashSet<(T) -> Unit>();
-			mListeners.put(T::class, list)
-		}
-
-		(list as HashSet<(T) -> Unit>).add(listener)
+	inner class HandlerEntry<T>(val handler : (T) -> Unit, val priority : Int = 0) {
 	}
 
-	inline fun <reified T> removeListener(noinline listener: (T) -> Unit) {
+	var _eventHandlers: HashMap<Any, Any> = HashMap()
 
-		var list = mListeners.get(T::class);
+	inline fun <reified T> addHandler(noinline listener: (T) -> Unit, priority : Int = 0) {
 
-		if (list == null) {
+		var handlers = _eventHandlers.get(T::class);
+
+		if (handlers == null) {
+			handlers = ArrayList<HandlerEntry<T>>()
+			_eventHandlers.put(T::class, handlers)
+		}
+		else {
+
+			// Check if handler is already registered. If yes, do nothing:
+			for(handlerEntry in handlers as ArrayList<HandlerEntry<T>>) {
+				if (handlerEntry.handler == listener) {
+					return
+				}
+			}
+		}
+
+		handlers.add(HandlerEntry(listener, priority))
+
+		Collections.sort(handlers, object : Comparator<HandlerEntry<T>> {
+			override fun compare(p0: HandlerEntry<T>, p1: HandlerEntry<T>): Int {
+
+				if (p0.priority > p1.priority) {
+					return -1
+				}
+				else if (p0.priority < p1.priority) {
+					return 1
+				}
+
+				return 0
+			}
+		})
+	}
+
+
+	inline fun <reified T> removeHandler(noinline listener: (T) -> Unit) {
+
+		var handlers = _eventHandlers.get(T::class);
+
+		if (handlers == null) {
 			return;
 		}
 
-		(list as HashSet<(T) -> Unit>).remove(listener)
+		var index = -1
+
+		for(handlerEntry in (handlers as ArrayList<HandlerEntry<T>>).withIndex()) {
+
+			if (handlerEntry.value.handler == listener) {
+				index = handlerEntry.index
+				break
+			}
+		}
+
+		if (index >= 0) {
+			handlers.removeAt(index)
+		}
 	}
+
 
 	inline fun <reified T> fire(event: T) {
  
-		var list = mListeners.get(T::class);
+		var handlers = _eventHandlers.get(T::class);
 
-		if (list == null) {
+		if (handlers == null) {
 			return;
 		}
 
-		for (listener in list as HashSet<(T) -> Unit>) {
-			listener(event)
+		for(handlerEntry in handlers as ArrayList<HandlerEntry<T>>) {
+			handlerEntry.handler(event)
 		}
 	}
 }
