ba72c38bf35f196cd06ed3333c525d9120c065aa
sbecht <sbecht>
2019-09-08 10:06:56 +0200

    - Fixed wrong handler function signature in removeHandler() method
    - Implemented a solution to prevent ConcurrentModificationExceptions when
      an event handler for a particular event type is added or removed by
      another handler of the same type.

--- src/EventBus.kt
+++ src/EventBus.kt
@@ -16,6 +16,8 @@ class EventBus {
 
 	var _eventHandlers: HashMap<Any, Any> = HashMap()
 
+
+
 	inline fun <reified T> addHandler(noinline listener: (T) -> Boolean, priority : Int = 0) {
 
 		var handlers = _eventHandlers.get(T::class);
@@ -52,7 +54,7 @@ class EventBus {
 	}
 
 
-	inline fun <reified T> removeHandler(noinline listener: (T) -> Unit) {
+	inline fun <reified T> removeHandler(noinline listener: (T) -> Boolean) {
 
 		var handlers = _eventHandlers.get(T::class);
 
@@ -84,7 +86,16 @@ class EventBus {
 			return;
 		}
 
-		for(handlerEntry in handlers as ArrayList<HandlerEntry<T>>) {
+		var handlersCopy = ArrayList<HandlerEntry<T>>()
+
+		// NOTE: Here we create a copy of list of handlers for the current event
+		// and run the handler calling loop on this copy instead of on the origin list.
+		// This prevents ConcurrentModificationExceptions in the case that a handler for the currently
+		// processed event is added or removed *within* the event firing loop (i.e. by one of the handlers).
+
+		handlersCopy.addAll(handlers as ArrayList<HandlerEntry<T>>)
+
+		for(handlerEntry in handlersCopy) {
 			var carryOn = handlerEntry.handler(event)
 
 			if (!carryOn) {
